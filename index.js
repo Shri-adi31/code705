59fb2e7c6cfa385c2b6f23fab99ccb48e8a059a1
shriman <shrimanaditya2.nayak@gmail.com>
2023-09-19 19:47:29 +0530

    feature 3

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
-console.log('hello worlds');
-console.log('version 7.0');
-=======
 console.log('hello world');
 console.log('version 8.5.0');
->>>>>>> feature2
+console.log ("the name is feature 3");
+
